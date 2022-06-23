694e1a06d68a2ae25f6a118377af5c47eb6343b4
HuynhEmCuong <huynhcuongem7597@gmail.com>
2022-06-23 16:16:53 +0700

    #add startup

--- MyEiu.API/Program.cs
+++ MyEiu.API/Program.cs
@@ -1,66 +1,84 @@
 
-using Microsoft.EntityFrameworkCore;
-using MyEiu.API.Configtion.Middleware;
-using MyEiu.Application.Services.App.Users;
-using MyEiu.Application.Services.Salary;
-using MyEiu.Automapper.Settings;
-using MyEiu.Data.EF.DbContexts;
-//using System.Text.Json.Serialization;
 
+using MyEiu.API;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Host.UseAutofac();
-
-
-// Add services to the container.
-//builder.Services.InstallServicesInAssembly(builder.Configuration);
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-//Ignore cycle loop if an object has number of children > 32
-builder.Services.AddControllers().AddNewtonsoftJson(options =>
-    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-);
-//add mapper
-builder.Services.AddSingleton(AutoMapperConfig.RegisterMappings().CreateMapper());
-builder.Services.AddSingleton(AutoMapperConfig.RegisterMappings());
-//add service
-//builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IPayrollService, PayrollService>();
-//add DBContext
-string EiuDbConnectionStr = builder.Configuration.GetConnectionString("WebEiuDbConnection");
-builder.Services.AddDbContext<WebEiuDbContext>(options => options.UseMySql(EiuDbConnectionStr, ServerVersion.AutoDetect(EiuDbConnectionStr)));
-
-string StaffEiuDbConnectionStr = builder.Configuration.GetConnectionString("StaffEiuDbConnection");
-builder.Services.AddDbContext<StaffEiuDbContext>(options => options.UseMySql(StaffEiuDbConnectionStr, ServerVersion.AutoDetect(StaffEiuDbConnectionStr)));
-
-builder.Services.AddDbContext<MobileAppDbContext>(options =>
-                       options.UseSqlServer(
-                           builder.Configuration.GetConnectionString("MobileAppDbConnection")));
-
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
 
-//app.UseMiddleware<ApiKeyMiddleware>();
-app.UseCors(x => x.AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowAnyOrigin());
 
-app.UseHttpsRedirection();
+//using Microsoft.EntityFrameworkCore;
+//using MyEiu.API.Configtion.Middleware;
+//using MyEiu.Application.Services.App.Users;
+//using MyEiu.Application.Services.Salary;
+//using MyEiu.Automapper.Settings;
+//using MyEiu.Data.EF.DbContexts;
+////using System.Text.Json.Serialization;
 
-app.UseAuthorization();
 
-app.MapControllers();
+//var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+
+
+//// Add services to the container.
+////builder.Services.InstallServicesInAssembly(builder.Configuration);
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+////Ignore cycle loop if an object has number of children > 32
+//builder.Services.AddControllers().AddNewtonsoftJson(options =>
+//    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+//);
+////add mapper
+//builder.Services.AddSingleton(AutoMapperConfig.RegisterMappings().CreateMapper());
+//builder.Services.AddSingleton(AutoMapperConfig.RegisterMappings());
+////add service
+////builder.Services.AddScoped<IUserService, UserService>();
+//builder.Services.AddScoped<IPayrollService, PayrollService>();
+////add DBContext
+//string EiuDbConnectionStr = builder.Configuration.GetConnectionString("WebEiuDbConnection");
+//builder.Services.AddDbContext<WebEiuDbContext>(options => options.UseMySql(EiuDbConnectionStr, ServerVersion.AutoDetect(EiuDbConnectionStr)));
+
+//string StaffEiuDbConnectionStr = builder.Configuration.GetConnectionString("StaffEiuDbConnection");
+//builder.Services.AddDbContext<StaffEiuDbContext>(options => options.UseMySql(StaffEiuDbConnectionStr, ServerVersion.AutoDetect(StaffEiuDbConnectionStr)));
+
+//builder.Services.AddDbContext<MobileAppDbContext>(options =>
+//                       options.UseSqlServer(
+//                           builder.Configuration.GetConnectionString("MobileAppDbConnection")));
+
+
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+////app.UseMiddleware<ApiKeyMiddleware>();
+//app.UseCors(x => x.AllowAnyHeader()
+//            .AllowAnyMethod()
+//            .AllowAnyOrigin());
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
 
---
+++ MyEiu.API/Startup.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyEiu.Application.Services.Salary;
+using MyEiu.Automapper.Settings;
+using MyEiu.Data.EF.DbContexts;
+
+namespace MyEiu.API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+           services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+           services.AddEndpointsApiExplorer();
+           services.AddSwaggerGen();
+            //Ignore cycle loop if an object has number of children > 32
+           services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+            //add mapper
+           services.AddSingleton(AutoMapperConfig.RegisterMappings().CreateMapper());
+           services.AddSingleton(AutoMapperConfig.RegisterMappings());
+            //add service
+            //builder.Services.AddScoped<IUserService, UserService>();
+           services.AddScoped<IPayrollService, PayrollService>();
+            //add DBContext
+            string EiuDbConnectionStr = Configuration.GetConnectionString("WebEiuDbConnection");
+           services.AddDbContext<WebEiuDbContext>(options => options.UseMySql(EiuDbConnectionStr, ServerVersion.AutoDetect(EiuDbConnectionStr)));
+
+            string StaffEiuDbConnectionStr = Configuration.GetConnectionString("StaffEiuDbConnection");
+           services.AddDbContext<StaffEiuDbContext>(options => options.UseMySql(StaffEiuDbConnectionStr, ServerVersion.AutoDetect(StaffEiuDbConnectionStr)));
+
+           services.AddDbContext<MobileAppDbContext>(options =>
+                                   options.UseSqlServer(
+                                       Configuration.GetConnectionString("MobileAppDbConnection")));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(x => x.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyOrigin());
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}"
+                );
+            });
+        }
+    }
+}
